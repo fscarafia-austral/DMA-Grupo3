3e5794b7c2528d72212995b2e00cce1aada3855d
Gugui <138422751+Gugui22@users.noreply.github.com>
2024-05-08 20:33:16 -0300

    arrays funcionando

--- select train and test.py
+++ select train and test.py
@@ -22,6 +22,9 @@ test_names = []
 training_files = []
 test_files = []
 
+pos_for_test = []
+pos_for_train = []
+
 # Nombres únicos de personas
 names_faces_uniq = np.unique(names_faces)
 
@@ -31,40 +34,57 @@ np.random.seed(1957)
 # Loop para seleccionar 3 fotos de cada uno p/el test y el resto quedan para training
 for i in names_faces_uniq:
     face_position = np.where(names_faces == i)[0]
-    pos_for_test = np.random.choice(face_position, 3, replace=False)
-    pos_for_train = np.setdiff1d(face_position, pos_for_test)
+    aux_for_test = np.random.choice(face_position, 3, replace=False)
+    aux_for_train = np.setdiff1d(face_position, aux_for_test)
+    pos_for_test.append(aux_for_test)
+    pos_for_train.append(aux_for_train)
+
+pos_for_test = np.concatenate(pos_for_test)  
+pos_for_train = np.concatenate(pos_for_train) 
     
-    test_faces.append(flat_faces_np[pos_for_test])
-    training_faces.append(flat_faces_np[pos_for_train])
+test_faces = flat_faces_np[pos_for_test]
+training_faces = flat_faces_np[pos_for_train]  
     
-    test_names.append(names_faces[pos_for_test])
-    training_names.append(names_faces[pos_for_train])
+test_names = names_faces[pos_for_test]
+training_names = names_faces[pos_for_train]
     
-    test_files.append(files_faces[pos_for_test])
-    training_files.append(files_faces[pos_for_train])
+test_files = files_faces[pos_for_test]
+training_files = files_faces[pos_for_train]
     
 print(test_files)
 
 #6 Pasamos de rango 0-255 a 0-1 por cuestiones numéricas
 
 # Change integers to 32-bit floating point numbers
-train_img_array_flatten = train_img_array_flatten.astype('float64')
 
-train_img_array_flatten = train_img_array_flatten/255.0
+training_faces = np.array(training_faces, dtype = object) 
+training_faces = training_faces.astype('float64')
+
+training_faces = training_faces/255.0
 
 # Change integers to 32-bit floating point numbers
-test_img_array_flatten = test_img_array_flatten.astype('float64')
 
-test_img_array_flatten = test_img_array_flatten/255.0
+test_faces = np.array(test_faces)
+test_faces = test_faces.astype('float64')
+
+test_faces = test_faces/255.0
 
 #7 Se ejecuta PCA y se calcula la cantidad de componentes
 # que explican el 90% de variabilidad 
 
-pca = PCA(n_components=.9, svd_solver = 'full')
-pca.fit(train_img_array_flatten)
+pca = PCA(n_components = 63, svd_solver = 'full')
+pca.fit(training_faces)
+traning_Z = pca.transform(training_faces)[:,3:] 
 
 print(f'Nro de componentes a utilizar en el PCA : {pca.n_components_}')
 
 #Algoritmo de Gustavo Denicolay
 # 16 Definiciones preliminares del algoritmo de Denicolay para RN con 1 capa oculta
 ##
+# backpropagation, just one hidden layer
+# lo hago con  matrices de pesos
+# puedo tener tantos inputs como quiera
+# puedo tener tantas neuronas ocultas como quiera
+# puedo tener tantas neuronas de salida como quiera
+# fuera de este codigo esta la decision que tomo segun el valor de salida de cada neurona de salida
+
